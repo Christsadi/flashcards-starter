ce948acbd91019e604e44236f4c04f4f2002f8f6
Christ Sadi <christsadi@gmail.com>
2022-11-25 05:57:55 +0200

    fix slice import

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { topicsSlice } from "../features/topics/topicsSlice";
+import topicsSlice from "../features/topics/topicsSlice";
 
 export default configureStore({
   reducer: {
